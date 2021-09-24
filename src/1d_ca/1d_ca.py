bde9946b1795c0cf3eb97a5abec40c959a5df694
guttulsrud <guttulsrud@outlook.com>
2021-09-24 19:33:18 +0200

    Stuff

--- src/1d_ca/1d_ca.py
+++ src/1d_ca/1d_ca.py
@@ -11,19 +11,24 @@ def render(vector):
     plt.show()
 
 
-def createCellularAutomaton(width, boundary_condition, update_rule):
+def createCellularAutomaton(width):
     vector = np.random.choice([0, 1], size=(width,))
 
-    vector = step(vector=vector, update_rule=update_rule)
-    render(vector)
-    pass
+    return vector
 
 
-def step(vector, update_rule):
+def step(vector, rule_lookup, boundary_condition):
+
+
+    print(vector)
+    print(rule_lookup)
     return vector
 
 
 def make_rule_lookup(rule_number):
+    if rule_number > 255:
+        return False
+
     binary_keys = [np.binary_repr(x, 3) for x in range(8)]
     binary_keys = np.flipud(binary_keys)
 
@@ -33,6 +38,26 @@ def make_rule_lookup(rule_number):
 
 
 boundary_condition = 'periodic'
-# createCellularAutomaton(width=12, boundary_condition=boundary_condition, update_rule=rule)
+rule_map = make_rule_lookup(91)
+ca = createCellularAutomaton(width=12)
 
-print(make_rule_lookup(110))
+step(ca, rule_map, boundary_condition)
+
+
+
+# np.random.choice([0, 1], size=(width,))
+
+
+# 1. Familiarize yourself with the models (CA, networks). DONE
+# 2. Implement in Python a cellular automaton which receives argument(s) to define its
+# rule.
+# 3. Familiarize yourself with the cart-pole balancing environment. You can install and
+# prepare this environment by following the instructions on
+# this link: https://gym.openai.com/docs/.
+#
+# 4. Come up with a method to encode input (environment observations) into the CA and
+# to decode the CA state into output (action).
+# 5. Come up with a fitness function that tracks the performance of the controller.
+# 6. Evolve the rule of the CA to improve its control of the cart.
+# 7. Expand it to a network model (a simple neural network model with binary neurons).
+# Then, evolve its parameters to improve the controller.
